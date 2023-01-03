46210a7e58252c3dc6defc4bde3061e8cb7ff0c5
Nik Graf <nik@nikgraf.com>
2023-01-03 07:04:57 +0100

    setup nextra

---
+++ README.md
@@ -0,0 +1,15 @@
+# Serenity Documentation
+
+## Install
+
+```sh
+pnpm i
+```
+
+## Development
+
+```sh
+pnpm dev
+```
+
+and visit localhost:3000
---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Serenity Documentation</span>,
+  project: {
+    link: "https://github.com/SerenityNotes/documentation",
+  },
+  docsRepositoryBase: "https://github.com/SerenityNotes/documentation",
+  footer: {
+    text: "Serenity Documentation",
+  },
+};
+
+export default config;
